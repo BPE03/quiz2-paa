6ca3cb9fab3a94e840d916af35e9fb2aa613f219
Bimantara Putra Ernandra <bima.pe02@gmail.com>
2025-05-28 12:07:06 +0700

    init

---
+++ Quiz2.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Main Window
+root = Tk()
+root.title('Guess the algorithm path')
+root.maxsize(900, 900)
+root.config(bg='black')
+
+font = ("Helvetica", 11)
+
+root.mainloop()
